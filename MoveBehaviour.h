63e631566181a375c713f689ac3e34061890c5bb
Sma44 <samsonma44@gmail.com>
2024-04-04 17:25:04 -0400

    combined behaviour classes

--- MoveBehaviour.h
+++ MoveBehaviour.h
@@ -5,7 +5,27 @@
 
 class MoveBehaviour{
   public:
-    virtual void move(int,int,int&,int&) =0;
+    virtual void move(int,int,int&,int&) = 0;
+};
+
+class EscapeeBehaviour : public MoveBehaviour {
+  public:
+    virtual void move(int,int,int&,int&);
+};
+
+class RescuedBehaviour : public MoveBehaviour{
+  public:
+    virtual void move(int,int,int&,int&);
+};
+
+class RescuerBehaviour : public MoveBehaviour{
+  public:
+    virtual void move(int,int,int&,int&);
+};
+
+class VillainBehaviour : public MoveBehaviour{
+  public:
+    virtual void move(int,int,int&,int&);
 };
 
 #endif

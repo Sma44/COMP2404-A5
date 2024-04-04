1c00ef9f7b09bbcdd3a7d7139c3f1febc135aecb
Sma44 <samsonma44@gmail.com>
2024-04-04 16:40:45 -0400

    implemented move function

--- Hero.cc
+++ Hero.cc
@@ -1,6 +1,25 @@
 #include "Hero.h"
 #include "EscapeeBehaviour.h"
+#include "RescuedBehaviour.h"
 
 Hero::Hero(char ava, int row, int col, std::string name) 
 : Participant(ava, row, col, new EscapeeBehaviour()), name(name), health(MAX_HEALTH), rescued(false) {}
 
+void Hero::incurDamage(Participant* p){
+  int damage = p->causeDamage();
+
+  if (damage = RESCUE){
+    rescued = true;
+    p->setBehavior(new RescuedBehaviour());
+    return;
+  }
+
+  health -= damage;
+
+  if (health <= 0){
+    health = 0;
+    dead = true;
+    avatar = '+';
+  }
+
+}

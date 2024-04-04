6d25fbd94b9ce30e59e06978778672d604473c84
Sma44 <samsonma44@gmail.com>
2024-04-04 16:14:23 -0400

    implmented constructor

--- Hero.cc
+++ Hero.cc
@@ -1,6 +1,6 @@
 #include "Hero.h"
+#include "EscapeeBehaviour.h"
 
-// add move behavior init for participant
 Hero::Hero(char ava, int row, int col, std::string name) 
-: Participant(ava, row, col), name(name), health(MAX_HEALTH), rescued(false) {}
+: Participant(ava, row, col, new EscapeeBehaviour()), name(name), health(MAX_HEALTH), rescued(false) {}
 

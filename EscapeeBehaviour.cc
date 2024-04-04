df7dc6831653dbc13a027da6308ac6ddf2e0f0b2
Sma44 <samsonma44@gmail.com>
2024-04-04 16:14:09 -0400

    added escapeed behaviour template

---
+++ EscapeeBehaviour.cc
@@ -0,0 +1,65 @@
+#include "EscapeeBehaviour.h"
+
+void EscapeeBehaviour::move(int oldRow, int oldCol, int& newRow, int& newCol){
+
+/*
+  int ranNum = random(2);
+
+    if (ranNum == LEFT){
+
+    }
+    else{
+
+    }
+
+
+  ranNum = random(10);
+  int ranDir = random(5);
+  int rowOffset = 0;
+  int colOffset = 0;
+
+  switch (ranNum) {
+  case 0: case 1: case 2: case 3: case 4:
+    rowOffset = -1;
+    break;
+  
+  case 5:
+    rowOffset = -2;
+    break;
+  
+  case 6: case 7: case 8: case 9:
+    rowOffset = 1;
+    break;
+  
+  default:
+    break;
+  }
+
+  switch (ranDir) {
+  case 0:
+    colOffset = -2;
+    break;
+  case 1:
+    colOffset = -1;
+    break;
+  case 2:
+    break;
+  case 3:
+    colOffset = 1;
+    break;
+  case 4:
+    colOffset = 2;
+    break;
+  default:
+    break;
+  }
+
+  if (p->validPos(pos->getRow() + rowOffset, pos->getCol() + colOffset)){
+    pos->set(pos->getRow() + rowOffset, pos->getCol() + colOffset);
+  }
+
+*/
+
+
+
+}

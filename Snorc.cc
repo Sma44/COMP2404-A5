22199e0ed68d1afbced8d0e01c369cd880a185d8
Sma44 <samsonma44@gmail.com>
2024-04-04 17:14:31 -0400

    added snorc class

---
+++ Snorc.cc
@@ -0,0 +1,8 @@
+#include "Snorc.h"
+
+Snorc::Snorc(int row,int col,int str) 
+: Participant('s',row,col,new VillainBehaviour()), strength(str) {}
+
+
+
+
---
+++ Snorc.h
@@ -0,0 +1,17 @@
+#ifndef SNORC_H
+#define SNORC_H
+
+#include "Participant.h"
+#include "VillainBehaviour.h"
+class Snorc : public Participant{
+  public:
+    Snorc(int=MAX_ROW, int=MAX_COL, int=2);
+    void incurDamage();
+    int causeDamage();
+    bool isSafe();
+
+  private:
+    int strength;
+};
+
+#endif

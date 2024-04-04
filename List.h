91c41a1281dedd0dd930f002adec8831e8c64173
Sma44 <samsonma44@gmail.com>
2024-04-04 13:29:24 -0400

    implemented list template

--- List.h
+++ List.h
@@ -19,5 +19,56 @@ class List {
     Node* head;
 };
 
+template<class T>
+List<T>::List() : head(nullptr) { }
+
+template<class T>
+List<T>::~List()
+{
+  Node* currNode = head;
+  Node* nextNode;
+
+  while (currNode != nullptr) {
+    nextNode = currNode->next;
+    delete currNode->data;
+    delete currNode;
+    currNode = nextNode;
+  }
+}
+
+template<class T>
+T* List<T>::operator+=(T* data){
+  Node* newNode = new Node();
+  newNode->data = data; 
+  newNode->next = nullptr;
+
+  Node* currNode = head;
+  Node* prevNode = nullptr;
+
+  while (currNode != null){
+    prevNode = currNode;
+    currNode = currNode->next;
+  }
+
+  if (prevNode == nullptr){
+    head = newNode;
+  } else {
+    prevNode->next = newNode;
+  }
+}
+
+template<class T>
+void List<T>::convertToArray(T* arr, int& size){
+  Node* currNode;
+  size = 0;
+
+  while(currNode != null){
+    arr[size] = currNode->data;
+    size++;
+    currNode = currNode->next;
+  }
+}
+
+
 #endif
 

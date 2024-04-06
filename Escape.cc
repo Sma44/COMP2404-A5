88caba0d1a90e7c86e0e350074cd76eeef001f76
Sma44 <samsonma44@gmail.com>
2024-04-06 17:17:30 -0400

    implemented print and isOver functions

--- Escape.cc
+++ Escape.cc
@@ -40,3 +40,77 @@ Escape::Escape(){
   arr += h1;
   arr += h2;
 }
+
+void Escape::runEscape(){
+
+  // bool flag = false;
+  // int spawn;
+  // while(!flag){
+  //   usleep(200000);
+  //   spawn = random(SNORC_SPAWN + NO_SPAWN);
+  //   if(spawn < SNORC_SPAWN){
+  //     spawnSnorc();
+  //   }
+
+  //   moveParticipants();
+  //   flag = isOver();
+
+  //   system("clear");
+  //   pit->print(&arr, h1, h2);
+  // }
+  // printOutcome();
+
+}
+
+bool Escape::isOver(){
+  return ((h1->isDead() || h1->isSafe()) && (h2->isDead() || h2->isSafe()));
+}
+
+bool Escape::withinBounds(int row, int col) {
+
+}
+
+void Escape::spawnSnorc(){
+  if (numSnorcs == MAX_SNORCS) { return; }
+
+  int col = random(MAX_COL);
+  int row = random(6) + MAX_HEIGHT;
+  int str = random(3) + MIN_STRENGTH;
+
+  arr+=(new Snorc(row, col, str));
+  numSnorcs++;
+
+}
+
+void Escape::spawnNinja(){
+
+}
+
+Participant* Escape::checkForCollision(Participant* p){
+
+}
+
+void Escape::moveParticipants(){
+
+}
+
+void Escape::printPit(){
+
+}
+
+void Escape::printOutcome(){
+   if (h1->isDead() && h2->isDead()){
+    cout << "Both Heroes have died." << endl;
+  }
+  else if(h1->isDead() && h2->isSafe()){
+    cout << "Only " << h2->getName() << " survived." << endl;
+  }
+  else if(h1->isSafe() && h2->isDead()){
+    cout << "Only " << h1->getName() << " survived." << endl;
+  }
+  else{
+    cout << "Both Heroes survived." << endl;
+  }
+  cout << setw(7) << left << h1->getName() << ": " << h1->getHealth() << endl;
+  cout << setw(7) << left << h2->getName() << ": " << h2->getHealth() << endl;
+}

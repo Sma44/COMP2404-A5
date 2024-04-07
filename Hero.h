11664d91c64418740795316f97e682726104a0a9
Sma44 <samsonma44@gmail.com>
2024-04-07 12:17:09 -0400

    changed default constructor value

--- Hero.h
+++ Hero.h
@@ -1,11 +1,11 @@
 #ifndef HERO_H
 #define HERO_H
 
-#include "Participant.h"
+#include "Participant.h"  
 
 class Hero : public Participant {
   public:
-  Hero(char='H', int=MAX_ROW, int=MAX_COL, std::string="N/A");
+  Hero(char=' ', int=MAX_ROW, int=MAX_COL, std::string="N/A");
   virtual void incurDamage(Participant*);
   virtual int causeDamage();
   bool isSafe();

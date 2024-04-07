a7524c45ca0f5996e8b2a4bace90482edc77ef26
Sma44 <samsonma44@gmail.com>
2024-04-07 13:40:49 -0400

    fixed avatar condition

--- Hero.cc
+++ Hero.cc
@@ -23,7 +23,14 @@ void Hero::incurDamage(Participant* p){
 }
 
 int Hero::causeDamage() { return 0; }
-bool Hero::isSafe() { return (row == 0); }
+
+bool Hero::isSafe() { 
+if (row == 0){
+  avatar = ' ';
+}  
+return (row == 0); 
+}
+
 std::string Hero::getName() { return name; }
 int Hero::getHealth() { return health; }
 bool Hero::isRescued() { return rescued; }
